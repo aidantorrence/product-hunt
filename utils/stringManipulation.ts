8d0949b1d9ecc2ef7071b5032276c9d37116ece9
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-15 19:46:36 -0500

    remove https://

--- utils/stringManipulation.ts
+++ utils/stringManipulation.ts
@@ -8,19 +8,17 @@ export function getWords(text: string | undefined, placeInText: number) {
     if (!text) return "";
 	if (text.includes("RT ")) return "RT";
 	const slice = splitByNCharacters(text, 8).slice(placeInText, placeInText + WORDS_PER_TWEET);
-	return slice
-		.map((word) => {
-			if (word.toLowerCase().includes("https://")) {
-				return "[LINK]";
-			} else {
-				return word;
-			}
-		})
-		.join(" ");
+	return slice.join(" ");
 }
 
 export function splitByNCharacters(text: string, n: number) {
-	let words = text.split(/\s+/);
+	let words = text.split(/\s+/).map((word) => {
+        if (word.toLowerCase().includes("https://")) {
+            return "[LINK]";
+        } else {
+            return word;
+        }
+    })
 	let result = [];
 	for (let i = 0; i < words.length; i++) {
         let start = 0;

50856e934c8a97cdd641609d758fc91eff176c4d
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-17 02:13:17 -0500

    x log

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -6,8 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  
-  console.log(req.body)
 
   const posts = await fetchTweets(req.body)
   res.status(200).json(posts)
